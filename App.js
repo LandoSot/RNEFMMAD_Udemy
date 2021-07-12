11799af35af50957aec4ea700fdc4ed99b6fe62b
Orlando Sotelo <danugfe@gmail.com>
2021-07-12 11:57:04 -0500

    TextInput
    Including the text input fields into the code
    to edit the showed text

--- App.js
+++ App.js
@@ -1,36 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   const [user, setUser] = useState ("Lando")
-  const [style, setStyle] = useState({form: "gordo", apareance: "feo"})
+  const [knowledge, setStudies] = useState({ study: "trader", phase: "entrenamiento" })
   const pressHandler = () =>{
     setUser("Elisa")
-    setStyle({form: "delgada", apareance: "hermosa"})
+    setStudies({ study: "repostera", phase: "modo profesional" })
   }
   
   return (
     <View style={styles.container}>
-      <Text>{user} es {style.form} y bastante {style.apareance}</Text>
+      <Text style={styles.mytext}>{user} es {knowledge.study} en {knowledge.phase}</Text>
+      <TextInput style={styles.textInput}
+        placeholder = "Enter the user name"
+        onChange ={e => setUser(e.target.value)}
+      />
+      <TextInput style={styles.textInput}
+        placeholder = "Enter the user studies"
+        onChange ={e => setStudies({ ...knowledge, study: e.target.value })}
+      />
+      <TextInput style={styles.textInput}
+        placeholder = "Enter the user grade"
+        onChange ={e => setStudies({ ...knowledge, phase: e.target.value })}
+      />
       <View style={styles.buttonStyle}>
-        <Button title="Update"
+        <Button title='Update'
           onPress = {pressHandler}
         />
       </View>
-      <StatusBar style="auto" />
+      <StatusBar style = 'auto' />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff',
+    flex: 1,
+    justifyContent: 'center'
   },
   buttonStyle: {
-    margin: 20
+    margin: 5
+  },
+  mytext: {
+    fontSize: 20
+  },
+  textInput: {
+    borderColor: 'black',
+    borderWidth: 1,
+    height: 50,
+    margin: 3,
+    padding: 5,
+    width: '70%'
   }
 });
