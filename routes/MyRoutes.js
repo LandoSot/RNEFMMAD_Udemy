7d19cf7171a409b459dcc1518d0e26b18a3ad1a7
Orlando Sotelo <danugfe@gmail.com>
2021-07-30 20:37:16 -0500

    style:Changing the style of headers
    Using the headers options to change the styles

--- routes/MyRoutes.js
+++ routes/MyRoutes.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import HomeR from '../screens/HomeR'
 import TaskR from '../screens/TasksR'
+import AboutR from '../screens/AboutR'
 import { createStackNavigator } from '@react-navigation/stack'
 
 const Stack = createStackNavigator ();
 
 const MyRoutes = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      /*screenOptions={{
+        headerStyle: {backgroundColor: 'grey'},
+        headerTintColor: 'white',
+        headerTitleStyle: 'bold'
+      }}*/
+    >
+
       <Stack.Screen name = 'Home' component={HomeR}/>
       <Stack.Screen name = 'Task' component={TaskR}/>
+      <Stack.Screen name = 'Abou' component={AboutR}/>
     </Stack.Navigator>
   )
 }
