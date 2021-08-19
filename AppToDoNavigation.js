bc75a8b8383185481fccf147dc458c2a2f2d1f22
Orlando Sotelo <danugfe@gmail.com>
2021-08-19 12:41:38 -0500

    Replacing App.js to continue the course
    To continue with the next section of the course,
    the current App.js will change the name to conserve it.

---
+++ AppToDoNavigation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import MyDrawer from './routes/MyDrawer';
+
+export default function App(){
+  return(
+    <NavigationContainer> 
+      <MyDrawer/>
+    </NavigationContainer>
+  );
+}
