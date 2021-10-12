b0e920a5845e2a8e6bc7d18ef6958b4d8bfdc7d2
Orlando Sotelo <danugfe@gmail.com>
2021-10-12 01:29:44 -0500

    Login Screen
    Create the login screen for users

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,36 +1,63 @@
-import React from 'react'
-import { View, Text, StatusBar } from 'react-native'
-import { TextInput } from 'react-native-paper'
+import React, { useState } from 'react'
+import { View, StatusBar } from 'react-native'
+import { Button, Text, TextInput } from 'react-native-paper'
 
-const LoginScreen = () => {
-  const [text, setText] = React.useState('')
+const LoginScreen = ({navigation}) => {
+  //const [text, setText] = React.useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
   return (
     <View style = {{flex: 1, alignItems:'center'}} >
       <StatusBar barStyle="light" />
       <View style = {{marginTop: '10%', width: '80%'}}>
       <Text style = {{fontWeight: 'bold', fontSize: 30}}>Inicia Sesión</Text>
       <Text>¡Hola! Es un gusto verte de nuevo.</Text>
-        <Text style = {{color: 'green'/*'#fcd580'*/, fontWeight: 'bold', marginTop: 50}}>Correo electrónico</Text>
+        <Text style = {{color: 'green'/*'#fcd580'*/, 
+          fontWeight: 'bold', marginTop: 50}}
+        >
+          Correo electrónico
+        </Text>
         <TextInput
           style = {{backgroundColor: null}}
-          value = {text}
+          value = {email}
+          onChangeText = {text => setEmail(text)}
           placeholder = 'ejemplo@email.com'
-          //placeholderTextColor = 'white'
+          /*placeholderTextColor = 'white'
           selectionColor = '#fcd580'
           outlineColor = '#fcd580'
-          underlineColor = 'black'
+          underlineColor = 'black'*/
         />
-        <Text style = {{color: 'green' /*'#fcd580'*/, fontWeight: 'bold', marginTop: 20}}>Contraseña</Text>
+        <Text style = {{color: 'green' /*'#fcd580'*/,
+          fontWeight: 'bold', marginTop: 20}}
+        >
+          Contraseña
+        </Text>
         <TextInput
           style = {{backgroundColor: null}}
-          value = {text}
-          placeholder = 'Contraseña?'
-          //placeholderTextColor = 'white'
+          //value = {text}
+          placeholder = 'micontraseña123'
+          value = {password}
+          onChangeText = {text => setPassword(text)}
+          /*placeholderTextColor = 'white'
           underlineColor = 'black'
           selectionColor = '#fcd580'
-          outlineColor = 'black'
+          outlineColor = 'black'*/
           secureTextEntry
         />
+      </View>
+      <View style = {{marginTop: '4%'}}>
+        <Button mode = 'contained' color = 'green'
+          onPress={() => console.log('Logeado')}
+          style = {{marginTop: '4%'}}
+        >
+          Iniciar Sesión
+        </Button>
+        <Button mode = 'contained' color = 'brown'
+          onPress={() => navigation.push('Register')}
+          style = {{marginTop: '4%'}}
+        >
+          Registrarse
+        </Button>
       </View>
     </View>
   )
